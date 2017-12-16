99b2f8708d18edb0f525ca3152bb2d1ea6cfdcda
Nick Karandejs <nick.karandejs@gmail.com>
2017-12-16 15:08:45 +0000

    Add a few functions on lists

--- src/stdlib.scm
+++ src/stdlib.scm
@@ -1,5 +1,5 @@
 (define (not x) (if x #f #t))
-(define (null? obj) (if (eqv obj '()) #t #f))
+(define (null? obj) (if (eqv? obj '()) #t #f))
 (define (list . objs) objs)
 (define (id obj) obj)
 
@@ -12,3 +12,38 @@
 (define negative? (curry < 0))
 (define (odd? num) (= 1 (mod num 2)))
 (define (even? num) (= 0 (mod num 2)))
+
+(define (foldr fun end lst)
+  (if (null? lst)
+    end
+    (fun (car lst) (foldr fun end (cdr lst)))))
+
+(define (foldl fun accum lst)
+  (if (null? lst)
+    accum
+    (foldl fun (fun accum (car lst)) (cdr lst))))
+
+(define fold foldl)
+(define reduce foldr)
+
+(define (unfold fun init pred)
+  (if (pred init)
+    (cons init '())
+    (cons init (unfold fun (fun init) pred))))
+
+(define (sum . lst) (fold + 0 lst))
+
+(define (product . lst) (fold * 1 lst))
+
+(define (and . lst) (fold && #t lst))
+
+(define (or . lst) (fold || #f lst))
+
+(define (max first . rest) (fold (lambda (p n) (if (> n p) n p)) first rest))
+(define (min first . rest) (fold (lambda (p n) (if (< n p) n p)) first rest))
+
+(define (length lst) (fold (lambda (x y) (+ x 1)) 0 lst))
+
+(define (copy lst) (foldr cons '() lst))
+
+(define (reverse lst) (fold (flip cons) '() lst))
